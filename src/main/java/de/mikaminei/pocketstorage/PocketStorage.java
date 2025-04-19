2227a44bcd88fd9c4b3ff702ee5f01ee44735352
Mika Blome <mail@mikablome.de>
2025-04-19 16:40:13 +0200

    Initial commit

---
+++ src/main/java/de/mikaminei/pocketstorage/PocketStorage.java
@@ -0,0 +1,15 @@
+package de.mikaminei.pocketstorage;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PocketStorage implements ModInitializer {
+
+	public static final String MOD_ID = "pocket-storage";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {}
+}
---
+++ src/main/java/de/mikaminei/pocketstorage/PocketStorageClient.java
@@ -0,0 +1,9 @@
+package de.mikaminei.pocketstorage;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PocketStorageClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {}
+}
